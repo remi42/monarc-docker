db3b05fcf974586ef92ce9b8ae04edaf98942521
Heiko Roth <heiko.roth@egotec.com>
2025-02-10 18:15:34 +0100

    db config

--- docker/ubuntu/database.local.php
+++ docker/ubuntu/database.local.php
@@ -1,22 +1,24 @@
+<?php
+
 return [
-'doctrine' => [
-'connection' => [
-'orm_default' => [
-'params' => [
-'host' => 'mariadb',
-'user' => 'monarc',
-'password' => 'demo',
-'dbname' => 'monarc_common',
-],
-],
-'orm_cli' => [
-'params' => [
-'host' => 'mariadb',
-'user' => 'monarc',
-'password' => 'demo',
-'dbname' => 'monarc_cli',
-],
-],
-],
-],
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'params' => [
+                    'host' => 'mariadb',
+                    'user' => 'monarc',
+                    'password' => 'demo',
+                    'dbname' => 'monarc_common',
+                ],
+            ],
+            'orm_cli' => [
+                'params' => [
+                    'host' => 'mariadb',
+                    'user' => 'monarc',
+                    'password' => 'demo',
+                    'dbname' => 'monarc_cli',
+                ],
+            ],
+        ],
+    ],
 ];
