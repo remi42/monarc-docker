5240ffd1a023f5c37b7d675f09ec2ae4349fa459
Heiko Roth <heiko.roth@egotec.com>
2025-02-10 17:36:44 +0100

    database connecttion

---
+++ docker/ubuntu/database.local.php
@@ -0,0 +1,22 @@
+return [
+'doctrine' => [
+'connection' => [
+'orm_default' => [
+'params' => [
+'host' => 'mariadb',
+'user' => 'monarc',
+'password' => 'demo',
+'dbname' => 'monarc_common',
+],
+],
+'orm_cli' => [
+'params' => [
+'host' => 'mariadb',
+'user' => 'monarc',
+'password' => 'demo',
+'dbname' => 'monarc_cli',
+],
+],
+],
+],
+];
