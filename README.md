4abb41dcffbca2cecc75f08a9c175c9312ce7641
Heiko Roth <heiko.roth@egotec.com>
2025-02-18 14:31:54 +0100

    db connection

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # monarc-docker
 docker-compose.yml environment to start monarc with docker
+
+## init
+
+```shell
+bash init.sh
+```
+
+## build
+
+Build container image without using cache
+
+```shell
+docker compose build --no-cache
+```
+
+## bash
+
+```shell
+docker compose exec monarc bash
+```
---
+++ docker/ubuntu/autoload/local.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ */
+
+$appdir = getenv('APP_DIR') ?: '/var/lib/monarc';
+
+$package_json = json_decode(file_get_contents('./package.json'), true);
+
+return [
+    'languages' => [
+        'fr' => [
+            'index' => 1,
+            'label' => 'Français',
+        ],
+        'en' => [
+            'index' => 2,
+            'label' => 'English',
+        ],
+        'de' => [
+            'index' => 3,
+            'label' => 'Deutsch',
+        ],
+        'nl' => [
+            'index' => 4,
+            'label' => 'Nederlands',
+        ],
+    ],
+
+    'defaultLanguageIndex' => 1,
+
+    'activeLanguages' => ['fr','en','de','nl','es','ro','it','ja','pl','pt','zh'],
+
+    'appVersion' => $package_json['version'],
+
+    'checkVersion' => true,
+    'appCheckingURL' => 'https://version.monarc.lu/check/MONARC',
+
+    'email' => [
+        'name' => 'MONARC',
+        'from' => 'info@monarc.lu',
+    ],
+
+    'instanceName' => 'Development', // for example a short URL or client name from ansible
+    'twoFactorAuthEnforced' => false,
+
+    'terms' => 'https://my.monarc.lu/terms.html',
+
+    'monarc' => [
+        'ttl' => 60,
+        'cliModel' => 'generic',
+    ],
+
+    'twoFactorAuthEnforced' => false,
+
+    'mospApiUrl' => 'https://objects.monarc.lu/api/',
+
+    'statsApi' => [
+        'baseUrl' => 'http://127.0.0.1:5005',
+        'apiKey' => '',
+    ],
+
+    'import' => [
+        'uploadFolder' => $appdir . '/data/import/files',
+        'isBackgroundProcessActive' => false,
+    ],
+
+    'export' => [
+        'defaultWithEval' => true,
+    ],
+
+    'captcha' => [
+        'enabled' => true,
+        'failedLoginAttempts' => 3,
+        'params' => [
+            'name' => 'MonarcCaptcha',
+            'font' => $appdir . '/data/fonts/arial.ttf',
+            'fontSize' => 30,
+            'height' => 60,
+            'wordLen' => 6,
+            'timeout' => 300,
+            'imgDir' => $appdir . '/public/captcha',
+            'imgUrl' => 'captcha/',
+        ],
+    ],
+
+    /* Custom configuration of the smtp. The example config below is to make Mailcatcher work for the dev env.
+    'smtpOptions' => [
+        'name' => 'localhost',
+        'host' => '127.0.0.1',
+        'port' => 1025,
+    ],
+    */
+];
